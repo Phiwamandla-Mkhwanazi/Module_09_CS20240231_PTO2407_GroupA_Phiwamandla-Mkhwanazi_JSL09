030c97d073e1ebd99c59840c82887ee587fea410
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-02-02 13:27:35 +0200

    Crypto: Display name and icon

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         return res.json()
     })
     .then(data => {
-        console.log(data)
+        document.getElementById("crypto-top").innerHTML = `<img src=${data.image.small} /> <span>${data.name}</span>`
     })
     .catch(err => console.error(err))
 /*-----------------------End CoinGecko Crytocurrency API---------------------------------------------*/    

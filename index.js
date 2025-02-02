e1f11ea9cd9502124278e95393cb657191b5fc41
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-02-02 15:20:29 +0200

    Update: using await

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*-----------------------Start Time---------------------------------------------*/
 function getCurrentTime() {
-    const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+    const date = new Date();
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"});
 }
 
 setInterval(getCurrentTime, 1000);
@@ -10,63 +10,41 @@ setInterval(getCurrentTime, 1000);
 
 
 /*-----------------------Start Unsplash Image API--------------------------------------------*/
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`;
-        document.getElementById('author').textContent = `By: ${data.user.name}`;
-    }) 
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+
+const pImage = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature");
+const pImageData = await pImage.json();
+document.body.style.backgroundImage = `url(${pImageData.urls.regular})`;
+document.getElementById("author").textContent = `By: ${pImageData.user.name}`;
+
 /*-----------------------End Unsplash Image API---------------------------------------------*/    
 
 
-/*----------------------Start CoinGekco Crytocurrency API----------------------------------------*/
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        console.log(res.status)
-        return res.json()
-    })
-    .then(data => {
-        
-        document.getElementById("crypto-top").innerHTML = 
-        `<img src=${data.image.small} /> 
-        <span>${data.name}</span>`;
+/*----------------------Start CoinGekco Cryptocurrency API----------------------------------------*/
 
-        document.getElementById("crypto").innerHTML += 
-        `
-        <p>🎯: $${data.market_data.current_price.usd}</p>
-        <p>👆: $${data.market_data.high_24h.usd}</p>
-        <p>👇: $${data.market_data.low_24h.usd}</p>`;
-    })
-    .catch(err => console.error(err))
+const pCrypto = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin");
+const pCryptoData = await pCrypto.json();
+document.getElementById("crypto-top").innerHTML = `
+    <img src=${pCryptoData.image.small} />
+    <span>${pCryptoData.name}</span>`;
+
+document.getElementById("crypto").innerHTML += `
+    <p>🎯: $${pCryptoData.market_data.current_price.usd}</p>
+    <p>👆: $${pCryptoData.market_data.high_24h.usd}</p>
+    <p>👇: $${pCryptoData.market_data.low_24h.usd}</p>`;
+
 /*-----------------------End CoinGecko Crytocurrency API---------------------------------------------*/    
 
 
 /*----------------------- Start Open Weather API-----------------------------------------*/    
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = 
-            `
-                <img src=${iconUrl} />
-                <p class="weather-temp" >${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>`;
-        })
-        .catch(err => console.error(err))
+navigator.geolocation.getCurrentPosition(async position => {
+    const pWeather = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+    const pWeatherData = await pWeather.json();    
+    const iconUrl = `http://openweathermap.org/img/wn/${pWeatherData.weather[0].icon}@2x.png`;
+    
+    document.getElementById("weather").innerHTML = 
+    `<img src=${iconUrl} />
+    <p class="weather-temp" >${Math.round(pWeatherData.main.temp)}º</p>
+    <p class="weather-city">${pWeatherData.name}</p>`;
 });
 
 /*-----------------------End Open Weather API---------------------------------------------*/    
